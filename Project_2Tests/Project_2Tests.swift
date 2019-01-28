c01b1c2d186be82256df6415a21e2899dbe0ca2c
Sia <sahornbuckle17@sias-macbook-pro.local>
2019-01-28 17:57:13 -0500

    Initial Commit

---
+++ Project_2Tests/Project_2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Project_2Tests.swift
+//  Project_2Tests
+//
+//  Created by Sia on 1/28/19.
+//  Copyright © 2019 Sia. All rights reserved.
+//
+
+import XCTest
+@testable import Project_2
+
+class Project_2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
