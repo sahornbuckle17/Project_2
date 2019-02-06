f99a18cadc2f74057fb7191451aa542d8d9b430d
Sia <sahornbuckle17@sias-macbook-pro.local>
2019-02-05 22:32:56 -0500

    flixster

---
+++ Project_2/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Project_2
+//
+//  Created by Sia on 2/5/19.
+//  Copyright © 2019 Sia. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
